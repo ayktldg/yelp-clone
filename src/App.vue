e7d3f68acfc0fe591e6a8034c79145e99ce2a5e6
ayktldg <aykutuludag26@gmail.com>
2020-10-03 10:46:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+}
+
+</style>
