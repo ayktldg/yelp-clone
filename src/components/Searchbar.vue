719788bfd15f264cafe49791c8da0b6832870311
ayktldg <aykutuludag26@gmail.com>
2020-10-04 01:50:34 +0300

    added searchbar

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex">
+    <b-form-input
+      v-model="searched.business"
+      placeholder="Find..."
+    ></b-form-input
+    ><b-form-input
+      v-model="searched.location"
+      placeholder="Location"
+    ></b-form-input>
+    <b-button variant="danger" @click="sendSearched"><b-icon icon="search"></b-icon></b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searched: {
+        business: "",
+        location: ""
+      }
+    };
+  },
+  methods: {
+      sendSearched(){
+          console.log(this.searched)
+      }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="home">
+    <div class="header d-flex align-items-center justify-content-center">
+      <div class="w-75 text-white text-center">
+        <h1>Yelp</h1>
+        <p><strong>Search every location you want to look</strong></p>
+      <Searchbar />
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-/* import HelloWorld from "@/components/HelloWorld.vue"; */
+ import Searchbar from "@/components/Searchbar.vue";
 
 export default {
   name: "Home",
   components: {
- 
+    Searchbar
+  },
+  data() {
+    return {
+  
+    };
   }
 };
 </script>
+<style scoped>
+.header {
+  background-image: url("../assets/restaurant.jpg");
+background-repeat: no-repeat; background-size: cover; background-position: center center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
