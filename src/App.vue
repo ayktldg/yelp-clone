76df2a731a75c66e20e0f4f13b79dd956c2baedf
Aykut <aykutuludag26@gmail.com>
2021-02-10 23:41:15 +0200

    added transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <transition name="fade">
     <router-view />
+     </transition>
   </div>
 </template>
 
@@ -16,4 +18,11 @@ a {
   color: black;
 }
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
